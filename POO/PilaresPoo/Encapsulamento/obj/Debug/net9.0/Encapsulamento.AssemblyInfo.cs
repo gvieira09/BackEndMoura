611ace55ecaaee990c38a9cb8b0dc882320878d7
Guilherme Vieira Carvlho <ogvieiraa@gmail.com>
2025-11-26 15:26:48 -0300

    novas atividades

--- POO/PilaresPoo/Encapsulamento/obj/Debug/net9.0/Encapsulamento.AssemblyInfo.cs
+++ POO/PilaresPoo/Encapsulamento/obj/Debug/net9.0/Encapsulamento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Encapsulamento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ba664ec74eada3697dd2fb8c5073776ee3efa8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14843b930639e803af3688fff52bcaa869432c68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Encapsulamento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Encapsulamento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
